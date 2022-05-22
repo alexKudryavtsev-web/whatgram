6bf8f162a72047810a51708e4336aff99953ff5f
alexKudryavtsev-web <qwertyshurazh@gmail.com>
2022-05-22 18:43:20 +0300

    added about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,9 +1,49 @@
+import { Box, Heading, Link, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 import WithIndent from "../components/ui/withIndent/WithIndent.jsx";
 
 function AboutPage() {
-  return <WithIndent>AboutPage</WithIndent>;
+  return (
+    <WithIndent>
+      <VStack align="flex-start" paddingTop={5}>
+        <Box>
+          <Text fontSize="2xl" fontWeight="bold">
+            "Whatgram" is free messenger, analogue of WhatsApp!
+          </Text>
+        </Box>
+        <Box>
+          <Text>
+            Use it for anything, because it has a minimalistic user-friendly
+            interface and reliability
+          </Text>
+        </Box>
+        <Box>
+          <Text>
+            <Link
+              color="blue.300"
+              href="https://github.com/alexKudryavtsev-web"
+              target="_blank"
+            >
+              author's github
+            </Link>
+          </Text>
+        </Box>
+
+        <Box>
+          <Text>
+            <Link
+              color="blue.300"
+              href="https://github.com/alexKudryavtsev-web/whatgram"
+              target="_blank"
+            >
+              source code
+            </Link>
+          </Text>
+        </Box>
+      </VStack>
+    </WithIndent>
+  );
 }
 
 export default AboutPage;
